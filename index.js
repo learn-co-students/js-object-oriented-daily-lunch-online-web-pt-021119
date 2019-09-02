19600bacaf0814519dd6af42aeb545de30444403
cyantis <cyantis@gmail.com>
2019-09-01 18:36:32 -0600

    Done.

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ class Neighborhood {
 
     store.neighborhoods.push(this);
   }
+
+  deliveries(){
+    return store.deliveries.filter( function(delivery) { return delivery.neighborhoodId === this.id }.bind(this) );
+  }
+
+  customers(){
+    return store.customers.filter( function(customer) { return customer.neighborhoodId === this.id }.bind(this) );
+  }
+
+  meals(){
+    const mealArr = this.deliveries().map(delivery => delivery.mealId);
+    const mealsArray = store.meals.filter(meal => mealArr.includes(meal.id));
+    return mealsArray;
+  }
+
 }
 
 class Customer {
@@ -22,6 +37,21 @@ class Customer {
 
     store.customers.push(this);
   }
+
+  deliveries(){
+    return store.deliveries.filter( function(delivery) { return delivery.customerId === this.id }.bind(this) );
+  }
+
+  meals(){
+    return this.deliveries().map( delivery => delivery.meal() );
+  }
+
+  totalSpent(){
+    const mealPrices = this.meals().map(meal => meal.price);
+    const total = mealPrices.reduce((acc, amount) => acc + amount);
+    return total;
+  }
+
 }
 
 class Meal {
@@ -32,6 +62,21 @@ class Meal {
 
     store.meals.push(this);
   }
+
+  deliveries(){
+    return store.deliveries.filter( function(delivery) { return delivery.mealId === this.id }.bind(this) );
+  }
+
+
+  customers(){
+    return this.deliveries().map( delivery => delivery.customer() );
+  }
+
+  static byPrice(){
+    const byPriceArr = [...store.meals].sort((a, b) => b.price - a.price );
+    return byPriceArr;
+  }
+
 }
 
 class Delivery {
@@ -42,5 +87,17 @@ class Delivery {
     this.id = ++deliveryId;
 
     store.deliveries.push(this);
+  }
+
+  meal(){
+    return store.meals.find( function(meal) { return meal.id === this.mealId }.bind(this) );
+  }
+
+  customer(){
+    return store.customers.find( function(customer) { return customer.id === this.customerId }.bind(this) );
+  }
+
+  neighborhood(){
+    return store.neighborhoods.find( function(neighborhood) { return neighborhood.id === this.neighborhoodId }.bind(this) );
   }
 }
